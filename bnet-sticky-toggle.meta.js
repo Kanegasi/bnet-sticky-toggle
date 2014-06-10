492d92a424a6853b8797de61427c60050426e253
Kanegasi <kanegasi@gmail.com>
2014-06-10 19:30:03 -0400

    Create bnet-sticky-toggle.meta.js

---
+++ bnet-sticky-toggle.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name          Battle.net Sticky Thread Toggle
+// @namespace     http://www.example.com/gmscripts
+// @description   Hides sticky threads in all Battle.net forums with toggle button
+// @version       1.0
+// @downloadURL   https://www.example.com/myscript.user.js
+// @updateURL     https://www.example.com/myscript.meta.js
+// @include       /^https?://\w+\.battle\.net\/\w+\/\w+\/forum\/\d+\/.*?$/
+// @grant         none
+// @icon          http://i.imgur.com/uWVXKdv.png
+// ==/UserScript==
