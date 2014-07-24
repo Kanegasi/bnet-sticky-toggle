52c6ba9f8be7f21156e0df35e993c48f28747092
Kanegasi <kanegasi@gmail.com>
2014-07-24 03:35:08 -0400

    update regex
    Found out I don't need to escape forward slashes.
    
    I also had to install GitHub and submit this change through it because
    for some reason, the site wouldn't let me edit user.js. It kept failing
    when I hit submit, seemingly refreshing the page with no error message,
    just a fresh edit page with none of my changes.

--- bnet-sticky-toggle.user.js
+++ bnet-sticky-toggle.user.js
@@ -2,10 +2,10 @@
 // @name          Battle.net Sticky Thread Toggle
 // @namespace     https://github.com/Kanegasi/bnet-sticky-toggle
 // @description   Hides sticky threads in all Battle.net forums with toggle button
-// @version       1.1
+// @version       1.2
 // @downloadURL   https://github.com/Kanegasi/bnet-sticky-toggle/raw/master/bnet-sticky-toggle.user.js
 // @updateURL     https://github.com/Kanegasi/bnet-sticky-toggle/raw/master/bnet-sticky-toggle.meta.js
-// @include       /^https?://(\w+\.)?battle\.net\/(\w+\/)?\w+\/forum\/\d+\/.*?$/
+// @include       /^https?://(\w+\.)?battle\.net/(\w+/)?\w+/forum/\d+/.*?$/
 // @grant         none
 // @icon          http://i.imgur.com/uWVXKdv.png
 // ==/UserScript==
