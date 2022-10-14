2ca4dd9d788d25e3aff6a546731ae2fe748c4155
Kanegasi <kanegasi@gmail.com>
2022-10-14 19:46:59 -0400

    require?

--- bnet-sticky-toggle.user.js
+++ bnet-sticky-toggle.user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
-// @name          Battle.net Sticky Thread Toggle
-// @namespace     https://github.com/Kanegasi/bnet-sticky-toggle
-// @version       2.5
-// @author        Kanegasi
-// @description   Hides sticky threads in all Battle.net forums with toggle button
-// @icon          http://i.imgur.com/uWVXKdv.png
-// @updateURL     https://github.com/Kanegasi/bnet-sticky-toggle/raw/master/bnet-sticky-toggle.meta.js
-// @downloadURL   https://github.com/Kanegasi/bnet-sticky-toggle/raw/master/bnet-sticky-toggle.user.js
-// @match         https://*.forums.blizzard.com/*/wow/c/*
-// @grant         none
+// @name         Battle.net Sticky Thread Toggle
+// @namespace    https://github.com/Kanegasi/bnet-sticky-toggle
+// @version      2.6
+// @author       Kanegasi
+// @description  Hides sticky threads in all Battle.net forums with toggle button
+// @icon         http://i.imgur.com/uWVXKdv.png
+// @updateURL    https://github.com/Kanegasi/bnet-sticky-toggle/raw/master/bnet-sticky-toggle.meta.js
+// @downloadURL  https://github.com/Kanegasi/bnet-sticky-toggle/raw/master/bnet-sticky-toggle.user.js
+// @match        https://*.forums.blizzard.com/*/wow/c/*
+// @require      https://releases.jquery.com/git/jquery-3.x-git.min.js
+// @grant        none
 // ==/UserScript==
+
+/* globals jQuery, $, waitForKeyElements */
 
 $(document).ready(function () {
 
