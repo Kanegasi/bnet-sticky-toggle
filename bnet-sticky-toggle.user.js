782bf5a4f4fe972c07abde5bead73b028f13512a
Kanegasi <kanegasi@gmail.com>
2022-10-15 18:12:53 -0400

    more mutationobserver

--- bnet-sticky-toggle.user.js
+++ bnet-sticky-toggle.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Battle.net Sticky Thread Toggle
 // @namespace    https://github.com/Kanegasi/bnet-sticky-toggle
-// @version      2.14
+// @version      2.15
 // @author       Kanegasi
 // @description  Hides sticky threads in all Battle.net forums with toggle button
 // @icon         http://i.imgur.com/uWVXKdv.png
@@ -15,43 +15,48 @@
 
 $(document).ready(function () {
 
-  $('.Navbar-items').append(
+  $(".Navbar-items").append(
     $('<a class="Navbar-item Navbar-link is-noSelect Navbar-stickytoggle stickytoggle" id="stickyhidden" data-index="4" data-name="stickytoggle" tabindex="0" data-analytics="global-nav" data-analytics-placement="Nav - StickyToggle"></a>').html(
       $('<div class="Navbar-label">Show Stickies</div>')
     )
   );
 
-  $('.stickytoggle').click(function() {
+  $(".stickytoggle").click(function() {
     var stickystatus = document.getElementById("stickyhidden");
     if (stickystatus) {
-      $('.stickytoggle').attr({ id: 'stickyshown' }).html(
+      $(".stickytoggle").attr({ id: "stickyshown" }).html(
         $('<div class="Navbar-label">Hide Stickies</div>')
       );
-      $('.pinned').attr({ style: 'visibility:visible' });
+      $(".pinned").attr({ style: "visibility:visible" });
     } else {
-      $('.stickytoggle').attr({ id: 'stickyhidden' }).html(
+      $(".stickytoggle").attr({ id: "stickyhidden" }).html(
         $('<div class="Navbar-label">Show Stickies</div>')
       );
-      $('.pinned').attr({ style: 'visibility:collapse' });
+      $(".pinned").attr({ style: "visibility:collapse" });
     }
   });
 
   setTimeout(function() {
-    $('.pinned').attr({ style: 'visibility:collapse' });
+    $(".pinned").attr({ style: "visibility:collapse" });
   }, 1000);
 
-  const observer = new MutationObserver(() => {
+  $.fn.classChange = function(cb) {
+    return $(this).each((_, el) => {
+      new MutationObserver(mutations => {
+        mutations.forEach(mutation => cb && cb(mutation.target, $(mutation.target).prop(mutation.attributeName)));
+      }).observe(el, {
+        attributes: true,
+        attributeFilter: ["class"]
+      });
+    });
+  }
+  $("body").classChange((el, newClass) => {
     var stickystatus = document.getElementById("stickyhidden");
     if (stickystatus) {
-      $('.pinned').attr({ style: 'visibility:collapse' });
+      $(".pinned").attr({ style: "visibility:collapse" });
     } else {
-      $('.pinned').attr({ style: 'visibility:visible' });
+      $(".pinned").attr({ style: "visibility:visible" });
     }
-  });
-
-  observer.observe(document.getElementById("container"), {
-    childList: true,
-    subtree: true
   });
 
 });
