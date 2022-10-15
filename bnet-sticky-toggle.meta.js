8e83552a0b076bde6368cdd384c8f67504ff2dd4
Kanegasi <kanegasi@gmail.com>
2022-10-15 15:00:18 -0400

    fix match

--- bnet-sticky-toggle.meta.js
+++ bnet-sticky-toggle.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Battle.net Sticky Thread Toggle
 // @namespace    https://github.com/Kanegasi/bnet-sticky-toggle
-// @version      2.8
+// @version      2.9
 // @author       Kanegasi
 // @description  Hides sticky threads in all Battle.net forums with toggle button
 // @icon         http://i.imgur.com/uWVXKdv.png
 // @updateURL    https://github.com/Kanegasi/bnet-sticky-toggle/raw/master/bnet-sticky-toggle.meta.js
 // @downloadURL  https://github.com/Kanegasi/bnet-sticky-toggle/raw/master/bnet-sticky-toggle.user.js
-// @match        https://*.forums.blizzard.com/*/wow/c/*
+// @match        https://*.forums.blizzard.com/*/*/c/*
 // @require      https://releases.jquery.com/git/jquery-3.x-git.min.js
 // @grant        none
 // ==/UserScript==
